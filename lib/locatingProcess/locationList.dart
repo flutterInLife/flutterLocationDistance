ea94d014d8c21a8390c60c70fe9694e61c4a477f
Samyush <samyushmaharjan@gmail.com>
2020-09-18 11:18:40 +0545

    finished up on locating shortest route

--- lib/locatingProcess/locationList.dart
+++ lib/locatingProcess/locationList.dart
@@ -8,10 +8,10 @@ class LocationList {
     LocationFormat(123.243, 321.24424),
     LocationFormat(090.243, 010.24424),
     LocationFormat(342.243, 434.24424),
-    LocationFormat(342.243, 434.24424),
-    LocationFormat(342.243, 434.24424),
-    LocationFormat(342.243, 434.24424),
-    LocationFormat(342.243, 434.24424),
+    LocationFormat(120.243, 80.24424),
+    LocationFormat(672.243, 1456.24424),
+    LocationFormat(123.243, 213.24424),
+    LocationFormat(655.243, 345.24424),
   ];
   void nextLocation() {
     if (_nextLocation < _locationList.length - 1) {
